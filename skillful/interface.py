6a64a41dd83ada68916a1d43c507d6b47597808c
bmweiner <bmweiner@users.noreply.github.com>
2016-04-23 11:44:49 -0400

    Updated __init__ methods to explicitly set args/attr.

--- skillful/interface.py
+++ skillful/interface.py
@@ -4,15 +4,19 @@ from __future__ import absolute_import, division, print_function
 import json
 import six
 
-class ValidateArgsMixin(object):
-    """Argument validation mixin class"""
-    def _validate_args(self, default_args, **kwargs):
-        # TODO: assert only valid args exist
-        # set args
-        for arg, val in six.iteritems(default_args):
-            setattr(self, arg, kwargs.get(arg, val))
+class DefaultAttrMixin(object):
+    """Sets default attributes"""
+    def _set_default_attr(self, default_attr):
+        """Sets default attributes when None.
 
-class Body(ValidateArgsMixin):
+        Args:
+            default_attr: dict. Key-val of attr, default-value.
+        """
+        for attr, val in six.iteritems(default_attr):
+            if getattr(self, attr, None) is None:
+                setattr(self, attr, val)
+
+class Body(DefaultAttrMixin):
     """Base HTTP body class"""
     def __repr__(self):
         return ('<' + self.__class__.__name__ + ' ' +
@@ -49,7 +53,7 @@ class Body(ValidateArgsMixin):
         """
         return _to_dict(self, drop_null, camel)
 
-class BodyChild(ValidateArgsMixin):
+class BodyChild(DefaultAttrMixin):
     """Base HTTP Body child class"""
     def __repr__(self):
         return '<' + self.__class__.__name__ + ' ' + str(self.__dict__) + '>'
@@ -69,12 +73,15 @@ class RequestBody(Body):
         session: obj. Context associated with the session see Session class.
         request: obj. Context association with the user, see Request class.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, version=None, session=None, request=None):
         """Inits a RequestBody class with placeholder params."""
-        default_args = dict(version=str(),
+        default_attr = dict(version=str(),
                             session=Session(),
                             request=Request())
-        self._validate_args(default_args, **kwargs)
+        self.version = version
+        self.session = session
+        self.request = request
+        self._set_default_attr(default_attr)
 
     def parse(self, body):
         """Parse JSON request, storing content in object attributes.
@@ -150,14 +157,20 @@ class Session(BodyChild):
             Application class.
         user: obj. Context associated with the user. See User class.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, new=None, session_id=None, attributes=None,
+                 application=None, user=None):
         """Inits a Session class with placeholder params."""
-        default_args = dict(new=bool(),
+        default_attr = dict(new=bool(),
                             session_id=str(),
                             attributes=dict(),
                             application=Application(),
                             user=User())
-        self._validate_args(default_args, **kwargs)
+        self.new = new
+        self.session_id = session_id
+        self.attributes = attributes
+        self.application = application
+        self.user = user
+        self._set_default_attr(default_attr)
 
 class Application(BodyChild):
     """Request context association with the application.
@@ -166,10 +179,11 @@ class Application(BodyChild):
         application_id: str. Intended application id for request. Used when
             Skill class initialized with application_id for validation.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, application_id=None):
         """Inits an Application class with placeholder params."""
-        default_args = dict(application_id=None)
-        self._validate_args(default_args, **kwargs)
+        default_attr = dict(application_id=None)
+        self.application_id = application_id
+        self._set_default_attr(default_attr)
 
 class User(BodyChild):
     """Request context associated with the user.
@@ -180,11 +194,13 @@ class User(BodyChild):
             disables then re-enables the skill.
         access_token: str. User token for a linked account, if configured.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, user_id=None, access_token=None):
         """Inits a User class with placeholder params."""
-        default_args = dict(user_id=str(),
+        default_attr = dict(user_id=str(),
                             access_token=str())
-        self._validate_args(default_args, **kwargs)
+        self.user_id = user_id
+        self.access_token = access_token
+        self._set_default_attr(default_attr)
 
 class Request(BodyChild):
     """Request context associated with all requests.
@@ -195,12 +211,15 @@ class Request(BodyChild):
         request_id: str. Unique identifier for request.
         timestamp: str. ISO 8601 formatted time and date when request sent.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, type_=None, request_id=None, timestamp=None):
         """Inits a Request class with placeholder params."""
-        default_args = dict(type=str(),
+        default_attr = dict(type=str(),
                             request_id=str(),
                             timestamp=str())
-        self._validate_args(default_args, **kwargs)
+        self.type = type_
+        self.request_id = request_id
+        self.timestamp = timestamp
+        self._set_default_attr(default_attr)
 
 class LaunchRequest(Request):
     """Request context associated with a launch request.
@@ -219,11 +238,12 @@ class IntentRequest(Request):
     Arguments:
         intent: obj. Context associated with the intent.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, intent=None):
         """Inits an IntentRequest class with placeholder params."""
         super(IntentRequest, self).__init__()
-        default_args = dict(intent=Intent())
-        self._validate_args(default_args, **kwargs)
+        default_attr = dict(intent=Intent())
+        self.intent = intent
+        self._set_default_attr(default_attr)
 
 class Intent(BodyChild):
     """Request context associated with an intent.
@@ -233,11 +253,13 @@ class Intent(BodyChild):
         slots: dict. Key-value pairs containing slot values per predefined
             schema.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, name=None, slots=None):
         """Inits an Intent class with placeholder params."""
-        default_args = dict(name=str(),
+        default_attr = dict(name=str(),
                             slots=dict())
-        self._validate_args(default_args, **kwargs)
+        self.name = name
+        self.slots = slots
+        self._set_default_attr(default_attr)
 
 class Slot(BodyChild):
     """Request context associated with an intent slot.
@@ -246,11 +268,13 @@ class Slot(BodyChild):
         name: str. Name of slot.
         value: str. Value of the slot.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, name=None, value=None):
         """Inits a Slot class with placeholder params."""
-        default_args = dict(name=str(),
+        default_attr = dict(name=str(),
                             value=str())
-        self._validate_args(default_args, **kwargs)
+        self.name = name
+        self.value = value
+        self._set_default_attr(default_attr)
 
 class SessionEndRequest(Request):
     """Request context associated with a session ended request.
@@ -261,11 +285,12 @@ class SessionEndRequest(Request):
         reason: str. Reason session ended, possible values are:
             ['USER_INITIATED', 'ERROR', 'EXCEEDED_MAX_REPROMPTS']
     """
-    def __init__(self, **kwargs):
+    def __init__(self, reason=None):
         """Inits a SessionEndRequest class with placeholder params."""
         super(SessionEndRequest, self).__init__()
-        default_args = dict(reason=str())
-        self._validate_args(default_args, **kwargs)
+        default_attr = dict(reason=str())
+        self.reason = reason
+        self._set_default_attr(default_attr)
 
 # implement response
 class ResponseBody(Body):
@@ -282,12 +307,15 @@ class ResponseBody(Body):
         response: obj. Context association with the response, see Response
             class.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, version=None, session_attributes=None, response=None):
         """Inits a ResponseBody class with placeholder params."""
-        default_args = dict(version='1.0',
+        default_attr = dict(version='1.0',
                             session_attributes=dict(),
                             response=Response())
-        self._validate_args(default_args, **kwargs)
+        self.version = version
+        self.session_attributes = session_attributes
+        self.response = response
+        self._set_default_attr(default_attr)
 
     def set_session_attribute(self, key, value):
         """Store session attribute.
@@ -409,13 +437,18 @@ class Response(BodyChild):
         reprompt: obj. Context associated with a reprompt.
         should_end_session: bool. If True, end the session.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, output_speech=None, card=None, reprompt=None,
+                 should_end_session=None):
         """Inits a Response class with placeholder params."""
-        default_args = dict(output_speech=OutputSpeech(),
+        default_attr = dict(output_speech=OutputSpeech(),
                             card=Card(),
                             reprompt=Reprompt(),
                             should_end_session=False)
-        self._validate_args(default_args, **kwargs)
+        self.output_speech = output_speech
+        self.card = card
+        self.reprompt = reprompt
+        self.should_end_session = should_end_session
+        self._set_default_attr(default_attr)
 
 class OutputSpeech(BodyChild):
     """Response context associated with output speech.
@@ -428,12 +461,15 @@ class OutputSpeech(BodyChild):
             with Speech Synthesis Markup Language. Cannot exceed 8,000
             characters.
         """
-    def __init__(self, **kwargs):
+    def __init__(self, type_=None, text=None, ssml=None):
         """Inits a OutputSpeech class with placeholder params."""
-        default_args = dict(type=str(),
+        default_attr = dict(type=str(),
                             text=str(),
                             ssml=str())
-        self._validate_args(default_args, **kwargs)
+        self.type = type_
+        self.text = text
+        self.ssml = ssml
+        self._set_default_attr(default_attr)
 
 class Card(BodyChild):
     """Response context associated with card to render.
@@ -448,14 +484,20 @@ class Card(BodyChild):
         text: str. Content of Standard type card.
         image: str. Context associated with a card image.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, type_=None, title=None, content=None, text=None,
+                 image=None):
         """Inits a Card class with placeholder params."""
-        default_args = dict(type=str(),
+        default_attr = dict(type=str(),
                             title=str(),
                             content=str(),
                             text=str(),
                             image=Image())
-        self._validate_args(default_args, **kwargs)
+        self.type = type_
+        self.title = title
+        self.content = content
+        self.text = text
+        self.image = image
+        self._set_default_attr(default_attr)
 
 class Image(BodyChild):
     """Response context associated with a card image.
@@ -466,11 +508,13 @@ class Image(BodyChild):
         large_image_url: str. URL of large image. Cannot exceed 2,000
             characters. Recommended pixel size: 1200w x 800h.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, small_image_url=None, large_image_url=None):
         """Inits an Image class with placeholder params."""
-        default_args = dict(small_image_url=str(),
+        default_attr = dict(small_image_url=str(),
                             large_image_url=str())
-        self._validate_args(default_args, **kwargs)
+        self.small_image_url = small_image_url
+        self.large_image_url = large_image_url
+        self._set_default_attr(default_attr)
 
 class Reprompt(BodyChild):
     """Response context associated with a reprompt.
@@ -478,10 +522,11 @@ class Reprompt(BodyChild):
     Attributes:
         output_speech: obj. Context associated with the output speech.
     """
-    def __init__(self, **kwargs):
+    def __init__(self, output_speech=None):
         """Inits a Reprompt class with placeholder params."""
-        default_args = dict(output_speech=OutputSpeech())
-        self._validate_args(default_args, **kwargs)
+        default_attr = dict(output_speech=OutputSpeech())
+        self.output_speech = output_speech
+        self._set_default_attr(default_attr)
 
 def _to_dict(obj, drop_null=True, camel=True):
     """Serialize obj as dict.
