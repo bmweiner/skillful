9d69d183ea33ac735f6afb02e9d97ee821c94ead
bmweiner <bmweiner@users.noreply.github.com>
2017-04-23 10:59:39 -0400

    Removed long_description, create .rst file.

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
 import sys
+import os
 from setuptools import setup
 
-from skillful import __version__
-from skillful import __doc__ as description
+__version__ = '0.7.1'
+__description__ = 'A Python package for building Alexa skills.'
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    dirname = os.path.dirname(__file__)
+    txt = pypandoc.convert(os.path.join(dirname, 'README.md'), 'rst')
+    with open(os.path.join(dirname, 'README.rst'), 'w') as f:
+        f.write(txt)
 except(IOError, ImportError):
-    try:
-        with open('README.md') as file:
-            long_description = file.read()
-    except(IOError):
-        long_description = 'See github repository.'
+    pass
 
 setup(name='skillful',
       version=__version__,
       author='bmweiner',
       author_email='bmweiner@users.noreply.github.com',
       url='https://github.com/bmweiner/skillful',
-      description=description,
-      long_description=long_description,
+      description=__description__,
       classifiers = [
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
--- skillful/__init__.py
+++ skillful/__init__.py
@@ -1,5 +1,3 @@
 """A Python package for building Alexa skills."""
 
 from .controller import Skill
-
-__version__ = '0.7'
